5b1c8ed817f2671af2a414895c0350274bc70aa8
Emmanuel <emmanuelm41@gmail.com>
2022-02-21 10:13:25 -0300

    attempt to fix a bug

--- app/src/common/app_main.c
+++ app/src/common/app_main.c
@@ -169,8 +169,10 @@ void app_init() {
     io_seproxyhal_init();
 
 #ifdef TARGET_NANOX
+#ifdef HAVE_BLE
     // grab the current plane mode setting
     G_io_app.plane_mode = os_setting_get(OS_SETTING_PLANEMODE, NULL, 0);
+#endif
 #endif // TARGET_NANOX
 
     USB_power(0);
