6e28941b8ee7bc3a210c1ec8810ba269fefc61a6
Carlos Medeiros <carlos.medeiros@zondax.ch>
2022-07-18 18:35:08 +0100

    support 2 key slots

--- app/src/crypto_store.c
+++ app/src/crypto_store.c
@@ -22,13 +22,19 @@
 #include "zxmacros_ledger.h"
 #include "os_io_seproxyhal.h"
 
+#define KEY_SLOT_1 0x00
+#define KEY_SLOT_2 0x01
+#define MASTERSEED_LEN 32
+
+static uint8_t slot_in_use = KEY_SLOT_1;
+
 typedef struct {
     uint8_t masterSeed[32];
     uint8_t pq[RSA_PRIME_LEN * 2];
     cx_rsa_4096_public_key_t rsa_pub;
     cx_rsa_4096_private_key_t rsa_priv;
     uint8_t initialized;
-} crypto_store_t;
+} crypto_store_t[2];
 
 crypto_store_t NV_CONST
 N_crypto_store_impl __attribute__ ((aligned(64)));
@@ -36,7 +42,7 @@ N_crypto_store_impl __attribute__ ((aligned(64)));
 
 typedef struct {
     uint8_t sig[RSA_MODULUS_LEN];
-} crypto_sig_t;
+} crypto_sig_t[2];
 
 crypto_sig_t NV_CONST
 N_crypto_sig_impl __attribute__ ((aligned(64)));
@@ -50,7 +56,7 @@ uint8_t signature_set;
 //////////////////////////////////////////////////////////////
 
 zxerr_t crypto_store_signature(uint8_t *sig){
-    MEMCPY_NV((void *)&N_crypto_sig.sig, sig, RSA_MODULUS_LEN);
+    MEMCPY_NV((void *)&N_crypto_sig[slot_in_use].sig, sig, RSA_MODULUS_LEN);
     signature_set = 1;
     return zxerr_ok;
 }
@@ -63,12 +69,12 @@ zxerr_t crypto_signature_part(uint8_t *sig, uint8_t index){
     if (signature_set != 1){
         return zxerr_invalid_crypto_settings;
     }
-    uint8_t *start = (uint8_t *)&N_crypto_sig.sig;
+    uint8_t *start = (uint8_t *)&N_crypto_sig[slot_in_use].sig;
     MEMCPY(sig, start + index*RSA_MODULUS_HALVE, RSA_MODULUS_HALVE);
     if (index == 1){
         uint8_t dummy[RSA_MODULUS_LEN];
         MEMZERO(dummy, RSA_MODULUS_LEN);
-        MEMCPY_NV((void *)&N_crypto_sig.sig, dummy, RSA_MODULUS_LEN);
+        MEMCPY_NV((void *)&N_crypto_sig[slot_in_use].sig, dummy, RSA_MODULUS_LEN);
         signature_set = 0;
     }
     return zxerr_ok;
@@ -101,21 +107,44 @@ zxerr_t crypto_deriveMasterSeed() {
                                         masterSeed,
                                         NULL, NULL, 0);
 
-    MEMCPY_NV((void*) &N_crypto_store.masterSeed, masterSeed, 32);
+    MEMCPY_NV((void*) &N_crypto_store[slot_in_use].masterSeed, masterSeed, 32);
     return zxerr_ok;
+}
+
+bool same_masterseed(uint8_t slot) {
+    uint8_t masterSeed[64];
+    uint32_t master_path[] = {
+            HDPATH_0_DEFAULT,
+            HDPATH_1_DEFAULT,
+            0x80000000u,
+            0x80000000u,
+            0x80000000u
+    };
+
+    if (slot > 2){
+        return zxerr_invalid_crypto_settings;
+    }
+
+    os_perso_derive_node_bip32_seed_key(HDW_NORMAL,
+                                        CX_CURVE_Ed25519,
+                                        master_path, HDPATH_LEN_DEFAULT,
+                                        masterSeed,
+                                        NULL, NULL, 0);
+    
+    return (MEMCMP((void*) &N_crypto_store[slot].masterSeed, masterSeed, 32) == 0);
 }
 
 zxerr_t crypto_derivePrime(uint8_t *prime, uint8_t index) {
     // Now derive p_seed and q_seed from the master seed
     uint8_t data_index[2] = {0, index};
-    uint8_t data[sizeof(data_index) + sizeof(N_crypto_store.masterSeed) + 1];
+    uint8_t data[sizeof(data_index) + sizeof(N_crypto_store[slot_in_use].masterSeed) + 1];
 
     //initialization of data to be hashed
     //Data: 0 || PRIME_INDEX (0 or 1) || MASTERSEED (32 bytes) || INDEX (0 - 255)
     MEMCPY(data, data_index, 2);
-    MEMCPY(data + 2, (void*) &N_crypto_store.masterSeed, sizeof(N_crypto_store.masterSeed));
+    MEMCPY(data + 2, (void*) &N_crypto_store[slot_in_use].masterSeed, sizeof(N_crypto_store[slot_in_use].masterSeed));
 
-    uint16_t index_location = 2 + sizeof(N_crypto_store.masterSeed);
+    uint16_t index_location = 2 + sizeof(N_crypto_store[slot_in_use].masterSeed);
 
     //Number of times we need to hash: RSA_PRIME_LEN * 8 bits / 256 bits
     uint16_t num_hashes = (RSA_PRIME_LEN*8)/256;
@@ -149,9 +178,12 @@ zxerr_t crypto_derivePrime(uint8_t *prime, uint8_t index) {
 //////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////
+bool crypto_store_slot_is_initialized(uint8_t slot) {
+    return N_crypto_store[slot].initialized;
+}
 
 bool crypto_store_is_initialized() {
-    return N_crypto_store.initialized;
+    return N_crypto_store[slot_in_use].initialized;
 }
 
 zxerr_t crypto_init_primes() {
@@ -178,7 +210,7 @@ zxerr_t crypto_init_primes() {
     view_message_show("Arweave", "Finding Q");
     UX_WAIT_DISPLAYED();
     cx_math_next_prime(pq + RSA_PRIME_LEN, RSA_PRIME_LEN);
-    MEMCPY_NV((void *) &N_crypto_store.pq, pq, RSA_PRIME_LEN * 2);
+    MEMCPY_NV((void *) &N_crypto_store[slot_in_use].pq, pq, RSA_PRIME_LEN * 2);
 
     return zxerr_ok;
 }
@@ -196,63 +228,93 @@ zxerr_t crypto_init_keys() {
                          (cx_rsa_public_key_t * ) & rsa_pub,
                          (cx_rsa_private_key_t * ) & rsa_priv,
                          exp_be_buf, 4,
-                         (const unsigned char *) &N_crypto_store.pq);
+                         (const unsigned char *) &N_crypto_store[slot_in_use].pq);
 
     view_message_show("Arweave", "store keys");
     UX_WAIT_DISPLAYED();
-    SET_NV((void *)&N_crypto_store.initialized, uint8_t, true)
-    MEMCPY_NV((void *)&N_crypto_store.rsa_pub, &rsa_pub, sizeof(rsa_pub));
-    MEMCPY_NV((void *)&N_crypto_store.rsa_priv, &rsa_priv, sizeof(rsa_priv));
+    SET_NV((void *)&N_crypto_store[slot_in_use].initialized, uint8_t, true)
+    MEMCPY_NV((void *)&N_crypto_store[slot_in_use].rsa_pub, &rsa_pub, sizeof(rsa_pub));
+    MEMCPY_NV((void *)&N_crypto_store[slot_in_use].rsa_priv, &rsa_priv, sizeof(rsa_priv));
 
     return zxerr_ok;
+}
+
+zxerr_t crypto_uninitialized_slot(uint8_t slot) {
+        uint8_t clean_init=0x00;
+        uint8_t clean_seed[MASTERSEED_LEN];
+
+        MEMZERO(clean_seed, MASTERSEED_LEN);
+        MEMCPY_NV((void *)&N_crypto_store[slot].masterSeed, &clean_seed, MASTERSEED_LEN);
+        MEMCPY_NV((void *)&N_crypto_store[slot].initialized, &clean_init, sizeof(clean_init));
+        
+        return zxerr_ok;
 }
 
 zxerr_t crypto_store_init() {
     // Derive master seed from mnemonic
     // Based on https://github.com/LedgerHQ/openpgp-card-app/blob/master/doc/developper/gpgcard3.0-addon.rst#how
 
-    if (!crypto_store_is_initialized()) {
-
 #ifdef APP_TESTING
         zemu_log_stack("-----");    // Crash device if testmode is enabled
-        SET_NV(&N_crypto_store.initialized, uint8_t, true)
+        slot_in_use=KEY_SLOT_1;
+        SET_NV(&N_crypto_store[slot_in_use].initialized, uint8_t, true)
         return zxerr_ok;
 #endif
-        view_message_show("Arweave", "Initializing...");
-        UX_WAIT_DISPLAYED();
 
-        view_message_show("Arweave", "Init seed");
-        UX_WAIT_DISPLAYED();
-        crypto_deriveMasterSeed();
-
-        view_message_show("Arweave", "Finding primes");
-        UX_WAIT_DISPLAYED();
-        zemu_log_stack("init::primes");
-        crypto_init_primes();
-
-        view_message_show("Arweave", "Finding keys");
-        UX_WAIT_DISPLAYED();
-        crypto_init_keys();
-
-        zemu_log_stack("initialized");
+    if (!crypto_store_slot_is_initialized(KEY_SLOT_1)) {
+        slot_in_use=KEY_SLOT_1;
+        CHECK_ZXERR(crypto_initialize_slot());
         return zxerr_ok;
-    }else{
-        //do nothing
+    } else if(same_masterseed(KEY_SLOT_1)) {
+        slot_in_use=KEY_SLOT_1;
+        return zxerr_ok;
+    } else if (!crypto_store_slot_is_initialized(KEY_SLOT_2)) {
+        slot_in_use=KEY_SLOT_2;
+        CHECK_ZXERR(crypto_initialize_slot());
+        return zxerr_ok; 
+    } else if(same_masterseed(KEY_SLOT_2)) {      
+        slot_in_use=KEY_SLOT_2;
+        return zxerr_ok;
+    } else {
+        CHECK_ZXERR(crypto_uninitialized_slot(KEY_SLOT_1));
+        CHECK_ZXERR(crypto_uninitialized_slot(KEY_SLOT_2));
+        slot_in_use=KEY_SLOT_1;
+        CHECK_ZXERR(crypto_initialize_slot());
         return zxerr_ok;
     }
+}
+
+zxerr_t crypto_initialize_slot() {
+    view_message_show("Arweave", "Initializing");
+    UX_WAIT_DISPLAYED();
+
+    view_message_show("Arweave", "Init seed");
+    UX_WAIT_DISPLAYED();
+    CHECK_ZXERR(crypto_deriveMasterSeed())
+
+    view_message_show("Arweave", "Finding primes");
+    UX_WAIT_DISPLAYED();
+    zemu_log_stack("init::primes");
+    CHECK_ZXERR(crypto_init_primes())
+
+    view_message_show("Arweave", "Finding keys");
+    UX_WAIT_DISPLAYED();
+    CHECK_ZXERR(crypto_init_keys())
+
+    return zxerr_ok;
 }
 
 cx_rsa_4096_public_key_t *crypto_store_get_pubkey() {
     if (!crypto_store_is_initialized()) {
         return NULL;
     }
-    return (cx_rsa_4096_public_key_t *) &N_crypto_store.rsa_pub;
+    return (cx_rsa_4096_public_key_t *) &N_crypto_store[slot_in_use].rsa_pub;
 }
 
 cx_rsa_4096_private_key_t *crypto_store_get_privkey() {
     if (!crypto_store_is_initialized()) {
         return NULL;
     }
-    return (cx_rsa_4096_private_key_t *)&N_crypto_store.rsa_priv;
+    return (cx_rsa_4096_private_key_t *)&N_crypto_store[slot_in_use].rsa_priv;
 }
 
